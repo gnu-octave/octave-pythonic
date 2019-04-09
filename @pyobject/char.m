c2c6204f4d5d430a86cd4cdb51f48d053810b602
Mike Miller <mtmiller@octave.org>
2019-04-09 14:52:14 -0700

    maint: update license header in all source files
    Change project name to Octave Pythonic in all license comment blocks.
    Use latest GPLv3 license text and formatting.

--- @pyobject/char.m
+++ @pyobject/char.m
@@ -1,20 +1,20 @@
 ## Copyright (C) 2016 Mike Miller
 ##
-## This file is part of Pytave.
+## This file is part of Octave Pythonic.
 ##
-## Pytave is free software; you can redistribute it and/or modify it
-## under the terms of the GNU General Public License as published by the
-## Free Software Foundation; either version 3 of the License, or (at your
-## option) any later version.
+## Octave Pythonic is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
 ##
-## Pytave is distributed in the hope that it will be useful, but WITHOUT
-## ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-## FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-## for more details.
+## Octave Pythonic is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
 ##
 ## You should have received a copy of the GNU General Public License
-## along with Pytave; see the file COPYING.  If not, see
-## <http://www.gnu.org/licenses/>.
+## along with Octave Pythonic; see the file COPYING.  If not, see
+## <https://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
 ## @documentencoding UTF-8
--- @pyobject/methods.m
+++ @pyobject/methods.m
@@ -1,21 +1,21 @@
 ## Copyright (C) 2016 Colin B. Macdonald
 ## Copyright (C) 2017 NVS Abhilash
 ##
-## This file is part of Pytave
+## This file is part of Octave Pythonic.
 ##
-## Pytave is free software; you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published
-## by the Free Software Foundation; either version 3 of the License,
-## or (at your option) any later version.
+## Octave Pythonic is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
 ##
-## This software is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty
-## of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
-## the GNU General Public License for more details.
+## Octave Pythonic is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
 ##
-## You should have received a copy of the GNU General Public
-## License along with this software; see the file COPYING.
-## If not, see <http://www.gnu.org/licenses/>.
+## You should have received a copy of the GNU General Public License
+## along with Octave Pythonic; see the file COPYING.  If not, see
+## <https://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
 ## @documentencoding UTF-8
